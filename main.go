716060200728b806b17601014784374fd7b6ad0e
Ryan Nickel <ryan@ryannickel.com>
2015-12-18 21:09:33 -0500

    Remove unused code

--- main.go
+++ main.go
@@ -2,22 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"os/exec"
-	"time"
 )
 
 const baseArticlePath string = "/Users/ryannickel/Documents/Pending/mrnickel.github.io/posts/"
 const baseTemplatePath string = "/Users/ryannickel/Documents/Pending/mrnickel.github.io/templates/"
 const basePublushPath string = "/Users/ryannickel/Documents/Pending/mrnickel.github.io/articles/"
 const baseRootPath string = "/Users/ryannickel/Documents/Pending/mrnickel.github.io/"
-
-// Article structs are very simple in that they only have content. i.e. what's in the markdown file.
-type Article struct {
-	Content     string
-	PublishTime time.Time
-}
 
 func main() {
 
@@ -39,36 +30,5 @@ func main() {
 	case "listdrafts":
 		ListDrafts()
 		return
-	}
-
-	// tmpCheckoutPath, _ := ioutil.TempDir(os.TempDir(), "mrnickel")
-	// articlePath := tmpCheckoutPath + "/posts/"
-
-	// fetchGitRepo("https://github.com/mrnickel/mrnickel.github.io.git", tmpCheckoutPath)
-	// articles := getArticles(articlePath)
-
-	// for _, article := range articles {
-	// 	post := parseArticle(articlePath + article.Name())
-	// 	article := Article{
-	// 		Content:     post,
-	// 		PublishTime: time.Now(),
-	// 	}
-
-	// 	generateArticle(tmpCheckoutPath, article)
-
-	// }
-
-	// os.RemoveAll(tmpCheckoutPath)
-}
-
-// Uses github to check out the specified git repo
-// Doesn't take into account any auth at the moment
-func fetchGitRepo(url string, checkoutPath string) {
-	cmd := exec.Command("git", "clone", url, ".")
-	cmd.Dir = checkoutPath
-
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
 	}
 }
