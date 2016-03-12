82c1aa9b8fe025f0b35466d798041b5bf40d9b1a
Ryan Nickel <ryan@ryannickel.com>
2016-03-12 00:22:44 -0500

    Add preview feature with web server

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/exec"
+	"path"
 )
 
 func main() {
@@ -70,17 +73,46 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		fmt.Printf("Launching your browser and go to http://localhost:8080/%s", p.HTMLPath())
+
+		cmd := exec.Command("open", fmt.Sprintf("http://localhost:8080/%s", p.HTMLPath()))
+		err = cmd.Run()
+		if err != nil {
+			panic(err)
+		}
+
+		http.HandleFunc("/", staticHandler)
+		http.ListenAndServe(":8080", nil)
+
 	default:
 		printHelp()
 	}
 }
 
 func printHelp() {
-	fmt.Println("You must choose one of the following options:\n")
+	fmt.Println("You must choose one of the following options:")
 	fmt.Println("publish \"Blog Title here\"")
 	fmt.Println("create \"Blog Title here\"")
 	fmt.Println("stats (this will list stats about your site)")
 	fmt.Println("listdrafts (this will list the titles of all your posts still in draft mode)")
 	fmt.Println("preview \"Blog Title here\"")
 	fmt.Println("newsite (creates a new site -- still needs to be implemented)")
+}
+
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open(r.URL.Path[1:])
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	fileStat, err := os.Stat(r.URL.Path[1:])
+	if err != nil {
+		panic(err)
+	}
+
+	_, filename := path.Split(r.URL.Path[1:])
+	t := fileStat.ModTime()
+	http.ServeContent(w, r, filename, t, file)
 }
